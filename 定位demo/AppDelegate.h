63010388128d40f1756427dc7176bc3c4ba54d4d
MacMini <ljy_huan@163.com>
2017-04-24 14:46:44 +0800

    Initial Commit

---
+++ 定位demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  定位demo
+//
+//  Created by citybao on 2017/4/24.
+//  Copyright © 2017年 citybao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
